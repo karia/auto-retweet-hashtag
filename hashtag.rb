8379687ff8573aa74e067b17ccee1a231f2bb66b
karia <karia@side2.net>
2019-06-17 03:58:07 +0900

    first commit

---
+++ hashtag.rb
@@ -0,0 +1,19 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |conf|
+  conf.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  conf.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+end
+
+query = "#ミコのえほん"
+search_count = 100
+
+result = client.search(query, count: search_count, result_type: "recent")
+
+result.take(search_count).each do |tw|
+  next if tw.full_text.start_with?("RT @")
+  puts "https://twitter.com/#{tw.user.screen_name}/status/#{tw.id}"
+  puts tw.created_at
+  puts tw.full_text
+end
+
